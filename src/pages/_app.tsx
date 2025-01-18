fc232aab4de7ecf684706606d4510a99f7ffbdd5
Gelzieny R. Martins <gelzieny@gmail.com>
2025-01-18 14:20:44 -0300

    Configurando fontes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  variable: '--font-roboto',
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${roboto.variable} $ --font-roboto`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
