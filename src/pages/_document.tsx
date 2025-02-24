3828485a99b13a41aa32e53ededf258ec031953d
Gelziney <gelzieny@gmail.com>
2025-02-24 20:55:49 -0300

    Criando favicon do projeto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-Br">
-      <Head />
+      <Head>
+        {/* Adicionando o favicon */}
+        <link rel="icon" type="image/png" href="favicon.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
